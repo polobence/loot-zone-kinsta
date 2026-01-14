26ac6879295ff17765dc9abf8bbe12abb120e1f1
polobence <polovicsbence@gmail.com>
2026-01-14 09:17:51 +0100

    create all products page

---
+++ src/pages/AllProductsPage.tsx
@@ -0,0 +1,10 @@
+import { ProductList } from "../components/ProductList";
+
+export function AllProductsPage() {
+  return (
+    <>
+      <h1>All Gaming Accessories</h1>
+      <ProductList />
+    </>
+  );
+}
