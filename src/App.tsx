0aec77c7dd3ddb51fad344bc10a9b5a571b02d75
polobence <polovicsbence@gmail.com>
2026-01-14 12:40:57 +0100

    protect cart page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { AllProductsPage } from "./pages/AllProductsPage";
 import { Layout } from "./components/layout/Layout";
 import { CartPage } from "./pages/CartPage";
+import { LoginPage } from "./pages/LoginPage";
+import { useAuth } from "./context/auth/useAuth";
 
 function App() {
+  const { user } = useAuth();
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<AllProductsPage />} />
-        <Route path="/cart" element={<CartPage />} />
+        <Route path="/cart" element={user ? <CartPage /> : <Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Layout>
   );
