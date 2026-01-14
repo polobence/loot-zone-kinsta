5a1ec1543e927240d13cf1b1359fc1483d4506d0
polobence <polovicsbence@gmail.com>
2026-01-14 09:26:20 +0100

    update app component with the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
-import { Button, StratusProvider } from "@kinsta/stratus";
+import { StratusProvider } from "@kinsta/stratus";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { AllProductsPage } from "./pages/AllProductsPage";
 
 function App() {
   return (
     <StratusProvider language="en">
-      <Button>Click Me</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AllProductsPage />} />
+        </Routes>
+      </BrowserRouter>
     </StratusProvider>
   );
 }
