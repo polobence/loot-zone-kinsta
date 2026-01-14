cfaaaa481307689af3afcc2cc79f6ebfe6f174f0
polobence <polovicsbence@gmail.com>
2026-01-14 11:52:34 +0100

    add route fo cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { AllProductsPage } from "./pages/AllProductsPage";
 import { Layout } from "./components/layout/Layout";
+import { CartPage } from "./pages/CartPage";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<AllProductsPage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
     </Layout>
   );
