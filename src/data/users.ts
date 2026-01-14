580625e7722eb1ac2fc0a01ba174396b2ea89e90
polobence <polovicsbence@gmail.com>
2026-01-14 12:28:35 +0100

    create filler users data

---
+++ src/data/users.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export const users: User[] = [
+  { id: "1", username: "alice", email: "alice@example.com", password: "1234" },
+  { id: "2", username: "bob", email: "bob@example.com", password: "password" },
+  { id: "4", username: "polobence", email: "bence@example.com", password: "cucc" },
+];
