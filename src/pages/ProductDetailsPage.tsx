eb43a1b5efed95a92ff6109d72c84e7e98527f91
polobence <polovicsbence@gmail.com>
2026-01-15 11:58:03 +0100

    create product details page

---
+++ src/pages/ProductDetailsPage.tsx
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import { products } from "../data/products";
+import { Button } from "@kinsta/stratus";
+import styled from "@emotion/styled";
+import { useCart } from "../context/cart/useCart";
+
+const Wrapper = styled.div`
+  max-width: 900px;
+  margin: 3rem auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  border-radius: 8px;
+`;
+
+export function ProductDetailsPage() {
+  const { productId } = useParams();
+  const { addToCart } = useCart();
+
+  const product = products.find((product) => product.id === productId);
+
+  if (!product) {
+    return <h2>Product not found</h2>;
+  }
+
+  return (
+    <Wrapper>
+      <Image src={product.imageUrl} alt={product.name} />
+
+      <div>
+        <h1>{product.name}</h1>
+        <p>{product.description}</p>
+        <h2>${product.price.toFixed(2)}</h2>
+
+        <Button onClick={() => addToCart(product)}>Add to Cart</Button>
+      </div>
+    </Wrapper>
+  );
+}
