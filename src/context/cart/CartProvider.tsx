ac434cbede1f18357ff5e6000c97ae8fb2abf9bb
polobence <polovicsbence@gmail.com>
2026-01-15 11:24:57 +0100

    restore cart on app load

--- src/context/cart/CartProvider.tsx
+++ src/context/cart/CartProvider.tsx
@@ -2,9 +2,13 @@ import { useState, type ReactNode } from "react";
 import type { CartItem } from "../../types/Cart";
 import type { Product } from "../../types/Product";
 import { CartContext } from "./CartContext";
+import { CART_STORAGE_KEY } from "../../constants/cart";
 
 export function CartProvider({ children }: { children: ReactNode }) {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCart = localStorage.getItem(CART_STORAGE_KEY);
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
 
   const addToCart = (product: Product) => {
     setCartItems((prev) => {
