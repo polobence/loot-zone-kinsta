2e8bc100291834e4f852830bec3daaa710259743
polobence <polovicsbence@gmail.com>
2026-01-15 11:59:14 +0100

    add route for product details

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Layout } from "./components/layout/Layout";
 import { CartPage } from "./pages/CartPage";
 import { LoginPage } from "./pages/LoginPage";
 import { useAuth } from "./context/auth/useAuth";
+import { ProductDetailsPage } from "./pages/ProductDetailsPage";
 
 function App() {
   const { user } = useAuth();
@@ -12,6 +13,7 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<AllProductsPage />} />
+        <Route path="/products/:productId" element={<ProductDetailsPage />} />
         <Route path="/cart" element={user ? <CartPage /> : <Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
