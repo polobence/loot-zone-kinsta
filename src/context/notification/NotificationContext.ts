353bb0347dc415fa99836b1e0ff19ff08b764d22
polobence <polovicsbence@gmail.com>
2026-01-16 10:36:36 +0100

    create notification context

---
+++ src/context/notification/NotificationContext.ts
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+export type NotificationType = "success" | "error";
+
+export interface Notification {
+  message: string;
+  type: NotificationType;
+}
+
+interface NotificationContextType {
+  showNotification: (message: string, type?: NotificationType) => void;
+}
+
+export const NotificationContext = createContext<NotificationContextType | null>(null);
