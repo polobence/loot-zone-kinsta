57e35fc9ce93e903d7c11767da227a31a6e5e1c4
polobence <polovicsbence@gmail.com>
2026-01-19 14:30:23 +0100

    add test cases to protect cart route

--- jest.config.ts
+++ jest.config.ts
@@ -7,18 +7,19 @@ const config: Config = {
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
-  transformIgnorePatterns: ["/node_modules/(?!(?:@kinsta/stratus|@emotion)/)"],
-  globals: {
-    "ts-jest": {
-      useESM: true,
-      tsconfig: {
-        jsx: "react-jsx",
-        esModuleInterop: true,
-      },
-    },
-  },
+  transformIgnorePatterns: ["/node_modules/(?!(?:@kinsta/stratus|@emotion|react-router-dom)/)"],
   transform: {
-    "^.+\\.[tj]sx?$": "ts-jest",
+    "^.+\\.[tj]sx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: {
+          jsx: "react-jsx",
+          esModuleInterop: true,
+          moduleResolution: "bundler",
+        },
+      },
+    ],
     "^.+\\.js$": "babel-jest",
   },
 };
